719708f6647f284904aff052a94ad35f70ab4bfc
Quami-source <emmanuelquami@outlook.com>
2021-03-18 02:46:23 +0000

    Finished the Navbar and the body

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -3,21 +3,36 @@ import '../../App.css';
 
 const Nav = ()=>{
     return(
-        <div className = 'Nav'>
-            <div className = 'Brand'>
-                <div>EA.</div>
+        <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor:'black',}}>
+            <a className="navbar-brand" href="#" >Quamies</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent" >
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="#">Action</a>
+                        <a className="dropdown-item" href="#">Another action</a>
+                        <div className="dropdown-divider"></div>
+                        <a className="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                
+                </ul>
                 
             </div>
-            <div className='LinksContainer'>
-                <div>
-                    <ul>
-                        <li><a href="#">Link</a></li>
-                        <li className="middle"><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        </nav>
     )
 }
 
--- src/components/nav/body/Body.js
+++ src/components/nav/body/Body.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import me from '../../../me.png';
+import me from '../../../image02.png';
 
 const Body = ()=>{
     return(
-        <div className = 'main-body'>
-            <div className = 'leftSpan'>
-                Hi, I'm Emmanuel
+        <div className='body-container'>
+            <div className='img-container'>
+                <div className='backrop'/>
+
+                <div className='imagedrop'>
+                    <img src={me} className='image-style' />
+                </div>
+
             </div>
-            <div className = 'imageSpan'>
-                <img src={me} width= "400" height = "400" />
+            <div className='text-container'>
+                <div className='text'>
+                    <div className='h-rule'/>
+                    <h2>Emmanuel Amoako</h2>
+                    <p style = {{color:'#6a6a6a',textTransform:'uppercase'}}>Software Developer</p>
+                    <p style = {{marginTop:30}} >I believe the wants and needs of every person is unlimited in relation to their problems. Hence, I like to learn and explore technologies capable of solving some of these problems. </p>
+                    <div className='social-media'>
+                        <div className='media-icon'>
+                            <a href="https://twitter.com/QuamiSource?s=09"><i class="fa fa-twitter"></i></a>
+                        </div>
+                        <div className='media-icon'>
+                            <a href="https://www.instagram.com/invites/contact/?i=sdwsgopstyyw&utm_content=10reyeq"><i class="fa fa-instagram"></i></a>
+                        </div>
+                        <div className='media-icon'>
+                            <a href="https://www.linkedin.com/in/emmanuel-amoako-352a7819b"><i class="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     )
