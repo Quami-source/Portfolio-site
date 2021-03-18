98cda86f8b6c0336055f7254da0043008d22648a
Quami-source <emmanuelquami@outlook.com>
2021-03-18 03:03:34 +0000

    created and added screenshots

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -15,20 +15,15 @@ const Nav = ()=>{
                         <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link" href="#">About me</a>
                     </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <div className="dropdown-divider"></div>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Projects</a>
                     </li>
-                
+                    
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
                 </ul>
                 
             </div>
