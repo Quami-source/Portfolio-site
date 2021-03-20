e45b6825e654fc8a5499396f58f83a1c168edbf1
Quami-source <emmanuelquami@outlook.com>
2021-03-20 22:29:28 +0000

    Implemented the email functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,49 @@
 import React from 'react'
+import emails from 'emailjs-com';
 
 export default function Contact({title}) {
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emails.sendForm('service_d55j5go', 'template_53t20k9', e.target, "user_LB9XEs57kY6vPIyxojZJW")
+            .then((result) => {
+                alert("Thank you for reaching me!")
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+    }
+
     return (
         <div className='about-container' id= 'contact'>
             <h3>Contact me</h3>
             <p style={{fontStyle:'italic', paddingRight:50}}>Send me an email with the link below.</p>
             <div className='h-rule'/>
-            <form action="#" >
+            <form onSubmit={sendEmail} >
                 <div class="row">
                     <div class="col">
-                        <input type="text" className="form-control" placeholder="First name"/>
+                        <input type="text" className="form-control" placeholder="Name" name="name"/>
                     </div>
                     <div class="col">
-                        <input type="text" className="form-control" placeholder="Last name"/>
+                        <input type="text" className="form-control" placeholder="Subject" name="subject" />
                     </div>
                 </div>
                 <div class="row">
                     <div class="col">
-                        <input type="email" className="form-control" placeholder="email"/>
+                        <input type="email" className="form-control" placeholder="Your Email" name ="email" />
                     </div>
                     <div class="col">
                         <input type="number" className="form-control" placeholder="Phone number"/>
                     </div>
                 </div>
-                <input type = "text" className="form-control" placeholder="Subject"/>
-                <button type = "submit" className="btn btn-info" style={{marginBottom:50}}>Submit</button>
+                <div className="form-group">
+                    <textarea className="form-control" name="message" rows="4" placeholder="Body of email">
+                       
+                    </textarea>
+                </div>
+                <input type = "submit" className="btn btn-info" value="Submit" style={{marginBottom:50}}/>
             </form>
         </div>
     )
